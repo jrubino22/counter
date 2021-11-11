c120aeb98067f05d32fa7768b6ad2b03ed421429
jrubino22 <93841868+jrubino22@users.noreply.github.com>
2021-11-10 16:43:40 -0800

    2nd commit

--- counter.js
+++ counter.js
@@ -1,31 +1,47 @@
-let target = "0";
+let target = 0;
 const dec = document.getElementById("decrease");
 const res = document.getElementById("reset");
 const inc = document.getElementById("increase");
-const scrn = document.getElementById("mainDiv")
+const scrn = document.getElementById("screenDiv")
 
-dec.addEventListener("click", function(target){ 
-    target -= 1
-})
-
-res.addEventListener("click", function(target){ 
-    target = 0
-})
-
-inc.addEventListener("click", function(target){ 
-    target += 1
-})
-
-function changeColor(target) {
+dec.addEventListener("click", function(){ 
+    target -= 1;
     if (target > 0) {
         scrn.style.color = "green";
     }
     else if (target < 0) {
         scrn.style.color = "red";
     }
-    else scrn.style.color = "yellow";
-}
+    else scrn.style.color = null;
+    scrn.textContent = target
+})
 
-function display(target) {
+res.addEventListener("click", function(){ 
+    target = 0;
+    if (target > 0) {
+        scrn.style.color = "green";
+    }
+    else if (target < 0) {
+        scrn.style.color = "red";
+    }
+    else scrn.style.color = null;
+    scrn.textContent = target
+})
+
+inc.addEventListener("click", function(){ 
+    target += 1;
+    if (target > 0) {
+        scrn.style.color = "green";
+    }
+    else if (target < 0) {
+        scrn.style.color = "red";
+    }
+    else scrn.style.color = null;
+    scrn.textContent = target
+})
+
+function changeColor(target) {
+
+
     scrn.textContent = target
 }
