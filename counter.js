60653b4f58f3818a4f0fb1a46783620329284437
jrubino22 <93841868+jrubino22@users.noreply.github.com>
2021-11-10 16:48:41 -0800

    3rd Commit

--- counter.js
+++ counter.js
@@ -6,30 +6,23 @@ const scrn = document.getElementById("screenDiv")
 
 dec.addEventListener("click", function(){ 
     target -= 1;
-    if (target > 0) {
-        scrn.style.color = "green";
-    }
-    else if (target < 0) {
-        scrn.style.color = "red";
-    }
-    else scrn.style.color = null;
-    scrn.textContent = target
+    changeColor();
+    scrn.textContent = target;
 })
 
 res.addEventListener("click", function(){ 
     target = 0;
-    if (target > 0) {
-        scrn.style.color = "green";
-    }
-    else if (target < 0) {
-        scrn.style.color = "red";
-    }
-    else scrn.style.color = null;
-    scrn.textContent = target
+    changeColor();
+    scrn.textContent = target;
 })
 
 inc.addEventListener("click", function(){ 
     target += 1;
+    changeColor();
+    scrn.textContent = target;
+})
+
+function changeColor() {
     if (target > 0) {
         scrn.style.color = "green";
     }
@@ -37,11 +30,4 @@ inc.addEventListener("click", function(){
         scrn.style.color = "red";
     }
     else scrn.style.color = null;
-    scrn.textContent = target
-})
-
-function changeColor(target) {
-
-
-    scrn.textContent = target
 }
